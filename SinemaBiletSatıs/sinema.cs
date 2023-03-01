417f96ed1e6cb01d63842792cf854e908ea5ff80
revit <revit@DESKTOP-I2H6A9U>
2023-03-01 13:53:44 +0300

    Add project files.

---
+++ SinemaBiletSatıs/sinema.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinemaBiletSatıs
+{
+    public class sinema
+    {
+        
+        
+            int toplamKoltukSayisi;
+            int bosKoltukSayisi;
+            double bakiye;
+            string salonNo;
+
+            const double TAM = 100.0;
+            const double INDIRIMLI = 75.0;
+
+            public sinema(string salonn, int koltuksayi)
+            {
+                toplamKoltukSayisi = koltuksayi;
+                salonNo = salonn;
+                bosKoltukSayisi = toplamKoltukSayisi;
+                bakiye = 0;
+
+            }
+
+            public void BiletSat(bool indirimli)
+            {
+                bosKoltukSayisi--;
+                if (indirimli)
+                    bakiye += INDIRIMLI;
+                else
+                    bakiye += TAM;
+            }
+
+            public void BiletIptal(bool indirimli)
+            {
+                bosKoltukSayisi++;
+                if (indirimli)
+                    bakiye -= INDIRIMLI;
+                else
+                    bakiye -= TAM;
+            }
+
+            public int BosKoltukOgren() 
+            {
+                return bosKoltukSayisi;
+            }
+
+            public double BakiyeOgren() 
+            {
+                return bakiye;
+            }
+    }
+}
